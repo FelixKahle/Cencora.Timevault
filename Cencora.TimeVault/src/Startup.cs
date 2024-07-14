1891613620691b20bc4f94a90c9f9534d1ebe933
Felix Kahle <felix.kahle21@gmail.com>
2024-07-15 01:54:32 +0200

    Authentication

--- Cencora.TimeVault/src/Startup.cs
+++ Cencora.TimeVault/src/Startup.cs
@@ -84,6 +84,7 @@ public class Startup
 
         app.UseHttpsRedirection();
         app.UseRouting();
+        app.UseAuthentication();
         app.UseAuthorization();
         app.UseEndpoints(endpoints =>
         {
